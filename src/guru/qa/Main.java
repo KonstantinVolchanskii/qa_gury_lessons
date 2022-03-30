b4072892114a80590abd2f9fd8c8b12c93ff4bca
KonstantinVolchanskii <konstantingrandf@gmail.com>
2022-03-30 14:00:45 +0300

    added new line branch

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello! QA Guru");
         System.out.println("Yep");
+        System.out.println("Branch");
     }
 }
